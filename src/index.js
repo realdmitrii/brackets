76d4be5defb416bf754fe679b5d316dd95b89c86
realdmitrii <conceptproect@gmail.com>
2023-10-29 15:56:46 +0500

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, conf) {
+  const obj = {};
+  const res = [];
+  conf.forEach(([open, close]) => obj[close] = open);
+  for (const x of str) {
+  if (res.length && res.at(-1) === obj[x]) {res.pop(x)}
+  else {res.push(x)}
+  } 
+  return !res.length;
 }
+
+
